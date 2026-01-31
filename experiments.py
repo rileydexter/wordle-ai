629862633dabb82ed91c36223c07759ec202eb3d
smiley texta <smileytexta314@gmail.com>
2026-01-31 11:16:36 +1100

    Computed runtime vs dictionary size and guesses vs dictionary size plots

--- experiments.py
+++ experiments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1ed11bdafb55d06144aa8ee7b68e0ef58c6640d489f437d737f12d1e9b88224
-size 3836
+oid sha256:22693d7d68bccc6b162f9218d453dd06320f2b58544e18f1cbe7ee90357c1520
+size 3976
